5300b93c84409e6bd0dc89e7c219e0b2c36e5a2b
geoffrey-crypto <geoffrey.kuhlmann@student.kuleuven.be>
2022-03-11 11:34:20 +0100

    test

--- app/src/main.c
+++ app/src/main.c
@@ -1,58 +1,69 @@
-/**
- * @file main.c
- * @version 0.1
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
-
 
 #include <stdint.h>
 #include <stm32l4xx.h>
 
+
+
+void delay(unsigned int n){
+    volatile unsigned int delay = n;
+    while (delay--);
+}
+
 int main(void) {
-	// Selecteren van HSI16
-	RCC->CCIPR &= ~RCC_CCIPR_USART2SEL_0;
-	RCC->CCIPR |= RCC_CCIPR_USART2SEL_1;
-	// Activeren klok
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN_Msk;
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN_Msk;
+
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;// het activeren van de GPIOB
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;// het activeren van de GPIOC
+
+    //Led1
 
 
-	GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE9_0;
-	GPIOC->MODER &= ~GPIO_MODER_MODE13_Msk;
-	GPIOC->MODER |= GPIO_MODER_MODE13_0;
 
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;
-	GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;
-volatile int i = 1000000;
-  while (i>0) {
-	  if ()//if button 1 is pushed down "setting in push"
-	  {
-		  //clock value is equal to a value
-		  GPIOB->ODR |= GPIO_ODR_ODE9; //led0 on for certain amount
-		  GPIOB->ODR &= ~GPIO_ODR_ODE9;
-		  GPIOC->ODR |= GPIO_ODR_ODE13; //led1 on for certain amount
-		  GPIOC->ODR &= ~GPIO_ODR_ODE13;
+    GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk; //blok B -> alle bits van pin9 op 0 zetten
+    GPIOB->MODER |= GPIO_MODER_MODE9_0; // bit 0 van pin9 hoog maken (=output)
+    GPIOB->OTYPER &= ~GPIO_OTYPER_OT9; // laag zetten voor push pull
 
-		  i--;
-	  }
-	  else
-	  {
-		  //clock value is equal to a value
-		  GPIOB->ODR |= GPIO_ODR_ODE9; //led0 on for certain amount
-		  GPIOB->ODR &= ~GPIO_ODR_ODE9;
-		  GPIOC->ODR |= GPIO_ODR_ODE13; //led1 on for certain amount
-		  GPIOC->ODR &= ~GPIO_ODR_ODE13;
-	  }
-  }
+    //Led2
+    GPIOC->MODER &= ~GPIO_MODER_MODE13_Msk;
+    GPIOC->MODER |= GPIO_MODER_MODE13_0;
+    GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;
+
+    //KnopA
+    GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk;
+
+    //KnopB
+    GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;
+
+    //pull up weerstand hoog zetten
+    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;
+    GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;
+    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
+    GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
+
+
+    while (1) {
+        if (!(GPIOB->IDR  & GPIO_IDR_ID13)) {
+            GPIOC->ODR |= GPIO_ODR_OD13;
+            GPIOB->ODR |= GPIO_ODR_OD9;
+            delay(500000);
+            GPIOC->ODR &= ~GPIO_ODR_OD13;
+            GPIOB->ODR &= ~GPIO_ODR_OD9;
+            delay(500000);
+        }
+
+        else if (!(GPIOB->IDR & GPIO_IDR_ID14)){
+            GPIOC->ODR |= GPIO_ODR_OD13;
+            GPIOB->ODR |= GPIO_ODR_OD9;
+            delay(1250000);
+            GPIOC->ODR &= ~GPIO_ODR_OD13;
+            GPIOB->BSRR = GPIO_BSRR_BR9;
+            delay(1250000);
+            }
+
+        else {
+            GPIOC->ODR &= ~GPIO_ODR_OD13;
+            GPIOB->BSRR = GPIO_BSRR_BR9;
+        }
+    }
+
+
 }
