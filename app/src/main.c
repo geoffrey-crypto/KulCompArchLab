ffe664b1053fcb3b9a998b7ef7fc0b96d4beed04
geoffrey-crypto <82381396+geoffrey-crypto@users.noreply.github.com>
2022-05-06 12:00:53 +0200

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -10,8 +10,12 @@ float waarde;
 float V;
 float R;
 int temperatuur;
+float t;
 int input_pot;
-
+int __io_putchar(int ch){
+			    while(!(USART1->ISR & USART_ISR_TXE));
+			    USART1->TDR = ch;
+			}
 void delay(unsigned int n) {
 	volatile unsigned int delay = n;
 	while (delay--) {
@@ -120,8 +124,26 @@ void SysTick_Handler(void) {
 int main(void) {
 
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
+	//Klok aanzetten van GPIO en UART blok
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+
+	//GPIO configureren en alternative mode aanzetten
+	GPIOA->MODER &= ~GPIO_MODER_MODE9_Msk;
+	GPIOA->MODER |=  GPIO_MODER_MODE9_1;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT9;
+	GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL9_Msk)) | (0x7 << GPIO_AFRH_AFSEL9_Pos);
+
+	GPIOA->MODER &= ~GPIO_MODER_MODE10_Msk;
+	GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL10_Msk)) | (0x7 << GPIO_AFRH_AFSEL10_Pos);
+
+	//UART configureren
+	USART1->CR1 = 0;
+	USART1->CR2 = 0;
+	USART1->CR3 = 0;
+	USART1->BRR = 417;
+	USART1->CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
 
 	//Potmeter
 	GPIOA->MODER &= ~GPIO_MODER_MODE1_Msk; //NTC configureren naar Analog
@@ -242,7 +264,8 @@ int main(void) {
 		R = (10000.0f * V) / (3.0f - V);
 		temperatuur = 10 * ((1.0f / ((logf(R / 10000.0f) / 3936.0f) + (1.0f / 298.15f))) - 273.15f);
 		delay(200);
-
+		t = temperatuur;
+		printf("%f\n\r",t/10);
 		//Instellen Timer, capture & compare
 		TIM16->CCMR1 &= ~TIM_CCMR1_CC1S;
 		TIM16->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1FE;
@@ -294,6 +317,8 @@ int main(void) {
 			R = (10000.0f * V) / (3.0f - V);
 			temperatuur = 10 * ((1.0f / ((logf(R / 10000.0f) / 3936.0f)+ (1.0f / 298.15f))) - 273.15f);
 			delay(200);
+			t = temperatuur;
+			printf("%f\n\r",t/10);
 
 			//reset kanalen
 			ADC1->SQR1 &= ~(ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_2| ADC_SQR1_SQ1_3);
